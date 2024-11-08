0af2570033b130d4aa8451b3ca0d5b51789e6059
u-chikara <r21022@osaka-pct.ac.jp>
2024-11-08 11:31:17 +0900

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,20 @@
 #include <Arduino.h>
 #include <Stepper.h>
 
+Stepper SM1=Stepper(200,0,4,2,1);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  SM1.setSpeed(120);
+  SM1.step(200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print("hello,world!\r\n");
-  delay(100);
+  //Serial.print("hello,world!\r\n");
+  SM1.step(200);
+  SM1.step(-200);
+  //delay(100);
 }
 
